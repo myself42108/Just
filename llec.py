29c65d97ee00c76ae043e2091fb4b3326a35d165
myself42108 <92208272+myself42108@users.noreply.github.com>
2021-10-09 15:14:52 +0530

    Create llec.py

---
+++ llec.py
@@ -0,0 +1,9 @@
+"""
+Problem Statement: Python program to check given two strings are Anagram of each other or not .
+    
+    An anagram of a string is an  another string that contains the same characters but the order of characters can be different.
+For example ,listen and silent are anagrams of each oter because all the characters in the string "listen" are present in string "silent".
+So, listen and silent are anagrams of each other. 
+     At First convert both strings to list of characters and sort them using sort() methid and check if both two list strings are equal print 
+    strings are anagrams of each other ,else print strings are not anagrams of eachother. 
+ """
